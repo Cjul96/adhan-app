6ca3e3635355659253cb8edc245b328e059202ae
Zulhaimie <zulhaimie.cs@gmail.com>
2025-05-09 17:28:06 +0800

    feat: update loading component

---
+++ src/components/Loading.svelte
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center justify-center h-64">
+    <div
+        class="w-16 h-16 border-4 border-blue-600 dark:border-blue-500 border-t-transparent dark:border-t-transparent rounded-full animate-spin mb-4"
+    ></div>
+    <p class="text-gray-600 dark:text-gray-300">Loading prayer times...</p>
+</div>
